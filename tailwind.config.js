82430086532d25716a80923afe6baa630dc93505
wojt707 <dolibog.wojtek@gmail.com>
2024-12-20 23:43:03 +0100

    tailwindcss import fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import * as tailwindAnimate from 'tailwindcss-animate'
+
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
@@ -33,5 +35,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindAnimate],
 }
